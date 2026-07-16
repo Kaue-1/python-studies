dfbebbcadf0f234d53597b98c6570f88e4363221
Kaue-1 <mendes.kaue@live.com>
2026-07-16 18:40:44 -0300

    indexing

---
+++ 01_basics/indexing.py
@@ -0,0 +1,16 @@
+credit_number = "1234-5678-1235-6548"
+
+
+#print(f"Your credit card is: XXXX-XXXX-XXXX-{credit_number[15:]}")
+
+#print(credit_number[0:4])
+#print(credit_number[5:9])
+#print(credit_number[5:])
+#print(credit_number[-3])
+#print(credit_number[::2])
+
+# reversing
+
+credit_number = credit_number[::-1]
+
+print(credit_number)
