d9b0db5210edf5b3aa3f8e2e5947ecbd0c80272e
Kaue-1 <mendes.kaue@live.com>
2026-08-01 00:01:21 -0300

    keypad

---
+++ 01_basics/2d_collection.py
@@ -0,0 +1,17 @@
+groceries = [["apple", "orange", "banana", "coconut"],
+["celery", "carrots", "potatoes"], 
+["chicken", "fish", "turkey"]]
+
+#fruits = ["apple", "orange", "banana", "coconut"]
+#vegetables = ["celery", "carrots", "potatoes"]
+#meats = ["chicken", "fish", "turkey"]
+
+
+#groceries = [fruits, vegetables, meats]
+
+
+for collection in groceries:
+    for food in collection: 
+        print(food, end=" ")
+
+print()        
